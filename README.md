ce898210bafd87d115d474004a95c5b040cd0ca9
@revakovsky <revakovsky@gmail.com>
2024-06-10 10:14:34 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# RunningTracker
+# Running Tracker
+
 An app that tracks your running and displays a running route on the map
+
+---
---
+++ app/src/main/java/com/revakovskyi/runningtracker/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.revakovskyi.runningtracker
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.revakovskyi.runningtracker.ui.theme.RunningTrackerTheme
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            RunningTrackerTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
