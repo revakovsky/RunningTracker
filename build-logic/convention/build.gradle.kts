5dcae10b3bf2953fc03915af9ecccc5e89ba3612
@revakovsky <revakovsky@gmail.com>
2024-06-19 09:45:44 +0300

    create a gradle plugin for the data-related modules - AndroidLibraryComposeConventionPlugin

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -28,5 +28,9 @@ gradlePlugin {
             id = "runningtracker.android.library"
             implementationClass = "AndroidLibraryConventionPlugin"
         }
+        register("androidLibraryCompose") {
+            id = "runningtracker.android.library.compose"
+            implementationClass = "AndroidLibraryComposeConventionPlugin"
+        }
     }
 }
---
+++ build-logic/convention/src/main/java/AndroidLibraryComposeConventionPlugin.kt
@@ -0,0 +1,21 @@
+import com.android.build.api.dsl.LibraryExtension
+import com.revakovskyi.convention.compose.configureAndroidCompose
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.getByType
+
+class AndroidLibraryComposeConventionPlugin : Plugin<Project> {
+
+    override fun apply(target: Project) {
+        target.run {
+            pluginManager.run {
+                apply("runningtracker.android.library")
+            }
+
+            extensions.getByType<LibraryExtension>().also { extension ->
+                configureAndroidCompose(extension)
+            }
+        }
+    }
+
+}
