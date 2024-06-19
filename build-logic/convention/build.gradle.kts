4d5cea969cc54ee017cfc8b92098e4c616785cf7
@revakovsky <revakovsky@gmail.com>
2024-06-19 09:19:28 +0300

    create a gradle plugin for the data-related modules - AndroidLibraryConventionPlugin

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -24,5 +24,9 @@ gradlePlugin {
             id = "runningtracker.android.application.compose"
             implementationClass = "AndroidApplicationComposeConventionPlugin"
         }
+        register("androidLibrary") {
+            id = "runningtracker.android.library"
+            implementationClass = "AndroidLibraryConventionPlugin"
+        }
     }
 }
