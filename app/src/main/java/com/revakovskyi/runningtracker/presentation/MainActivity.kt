da0bc03fc6f2770bb57f5d9ea4ec766b6ac966bf
@revakovsky <revakovsky@gmail.com>
2024-07-20 12:24:52 +0300

    fix a bug with splash screen overview

--- app/src/main/java/com/revakovskyi/runningtracker/presentation/MainActivity.kt
+++ app/src/main/java/com/revakovskyi/runningtracker/presentation/MainActivity.kt
@@ -20,10 +20,10 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setUpImeWindowInsets()
         installSplashScreen().apply {
             setKeepOnScreenCondition { viewModel.state.isCheckingAuthInfo }
         }
+        setUpImeWindowInsets()
         setContent {
             RunningTrackerTheme {
                 Surface(
