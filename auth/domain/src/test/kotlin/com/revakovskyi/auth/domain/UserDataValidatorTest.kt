67a0860e80b86da26086b4d497ee729c2eeb81ae
@revakovsky <revakovsky@gmail.com>
2024-12-14 14:41:53 +0200

    create first unit test

--- auth/domain/src/test/kotlin/com/revakovskyi/auth/domain/UserDataValidatorTest.kt
+++ auth/domain/src/test/kotlin/com/revakovskyi/auth/domain/UserDataValidatorTest.kt
@@ -1,3 +1,40 @@
 package com.revakovskyi.auth.domain
 
-class UserDataValidatorTest
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import assertk.assertions.isTrue
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+
+class UserDataValidatorTest {
+
+    private lateinit var userDataValidator: UserDataValidator
+
+    @BeforeEach
+    fun setUp() {
+        userDataValidator = UserDataValidator(
+            validator = object : PatternValidator {
+                override fun matches(value: String): Boolean {
+                    return true
+                }
+
+            }
+        )
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "Test12345, true",
+        "test12345, false",
+        "12345, false",
+        "Test-1234, true",
+        "TEST12345, false",
+    )
+    fun testValidatePassword(password: String, expectedIsValid: Boolean) {
+        val state = userDataValidator.isValidPassword(password)
+        assertThat(state.isValidPassword).isEqualTo(expectedIsValid)
+    }
+
+}
